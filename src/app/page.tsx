0c1c0f5fcd6b26310f1b44b0efd0ec8585584636
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-12-02 20:27:28 +0500

    card wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import { LoginButton } from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button size="lg" variant="outline">
-      Hello Shadcn
-    </Button>
+    <main className="flex h-full flex-col items-center justify-center bg-sky-500">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication example</p>
+        <div>
+          <LoginButton>
+            <Button variant={"secondary"}>Sign in</Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }

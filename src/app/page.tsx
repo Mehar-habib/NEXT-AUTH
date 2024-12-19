8005722b2a3f758f62f4fe8f5daf2966e4a2e117
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-12-19 20:46:38 +0500

    style(ui): complete home page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
-    <main>
-      <Button>Test</Button>
+    <main className="flex h-full flex-col items-center justify-center bg-[conic-gradient(at_top, _var(--tw-gradient-stops))] from-gray-950 via-gray-400 to-gray-950">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn("text-6xl font-semibold text-gray-800 drop-shadow-md")}
+        >
+          🔏 Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication system</p>
+        <div>
+          <Button variant={"secondary"} size={"lg"}>
+            Sign in
+          </Button>
+        </div>
+      </div>
     </main>
   );
 }

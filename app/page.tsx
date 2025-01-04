f503730e5450cdd42ef46814c64667a93d79fad2
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2025-01-04 15:46:53 +0500

    implementing login part 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import LoginButton from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Poppins } from "next/font/google";
@@ -23,9 +24,11 @@ export default function Home() {
           A simple authentication service for Next.js
         </p>
       </div>
-      <Button variant={"secondary"} size={"lg"}>
-        Sign in
-      </Button>
+      <LoginButton mode="modal">
+        <Button variant={"secondary"} size={"lg"}>
+          Sign in
+        </Button>
+      </LoginButton>
     </main>
   );
 }

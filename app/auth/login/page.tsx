844ac26c6b6c58b824d91cfe1c20aeae6f763e9c
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2025-01-04 16:48:09 +0500

    login part 3

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,3 +1,9 @@
+import LoginForm from "@/components/auth/login-form";
+
 export default function LoginPage() {
-  return <div>Login page</div>;
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
 }

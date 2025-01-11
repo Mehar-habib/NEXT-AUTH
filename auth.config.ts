c145fba019b7634a252266e446004e4f9234a993
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2025-01-11 18:38:21 +0500

    google, github authentication

--- auth.config.ts
+++ auth.config.ts
@@ -3,34 +3,35 @@ import type { NextAuthConfig } from "next-auth";
 import { LoginSchema } from "./schemas";
 import { getUserByEmail } from "./data/user";
 import bcrypt from "bcryptjs";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
-// NextAuth configuration for setting up the authentication flow
 export default {
   providers: [
-    // Using the Credentials provider for handling custom authentication
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
-      // `authorize` function defines the custom authentication logic
       authorize: async (credentials) => {
-        // Validating credentials using the LoginSchema
         const validatedFields = LoginSchema.safeParse(credentials);
         if (validatedFields.success) {
-          // Destructuring the email and password from the validated data
           const { email, password } = validatedFields.data;
 
-          // Fetching the user by email from the database
           const user = await getUserByEmail(email);
-          if (!user || !user.password) return null; // If no user or no password found, return null
+          if (!user || !user.password) return null;
 
-          // Comparing the entered password with the hashed password stored in the database
           const passwordMatch = await bcrypt.compare(password, user.password);
           if (passwordMatch) {
-            // If passwords match, return the user object (authenticated successfully)
             return user;
           }
         }
-        // If authentication fails, return null
         return null;
       },
     }),
   ],
-} satisfies NextAuthConfig; // Ensures the configuration satisfies the NextAuthConfig type
+} satisfies NextAuthConfig;

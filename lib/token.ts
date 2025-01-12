56b8612647076af7280e1ceb799f0f1ed4d705fb
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2025-01-12 20:30:24 +0500

    2FA

--- lib/token.ts
+++ lib/token.ts
@@ -1,17 +1,28 @@
-import { getVerificationTokenByEmail } from "@/data/verficiation-token"; // Function to fetch an existing verification token by email
-import { v4 as uuidv4 } from "uuid"; // Library to generate unique tokens
-import { db } from "./db"; // Prisma client instance for database operations
-import { getPasswordResetTokenByEmail } from "@/data/password-reset-token"; // Function to fetch an existing password reset token by email
+import { getVerificationTokenByEmail } from "@/data/verficiation-token";
+import { v4 as uuidv4 } from "uuid";
+import { db } from "./db";
+import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
+import crypto from "crypto";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 
-/**
- * Generates a password reset token for the provided email.
- * - Generates a unique token using UUID.
- * - Deletes any existing token for the email to avoid duplicates.
- * - Saves the new token to the database with an expiration time.
- *
- * @param {string} email - The email address to associate with the password reset token.
- * @returns {Promise<object>} The newly created password reset token object.
- */
+export const generateTwoFactorToken = async (email: string) => {
+  const token = crypto.randomInt(10_000, 1_000_000).toString();
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+  const existingToken = await getTwoFactorTokenByEmail(email);
+  if (existingToken) {
+    await db.twoFactorToken.delete({
+      where: { id: existingToken.id },
+    });
+  }
+  const twoFactorToken = await db.twoFactorToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+  return twoFactorToken;
+};
 export const generatePasswordResetToken = async (email: string) => {
   const token = uuidv4(); // Generate a unique token
   const expires = new Date(new Date().getTime() + 3600 * 1000); // Set token expiration to 1 hour from now
